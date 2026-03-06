68206efe206a2dfc669d8f23f2253793ab0726a0
Prathmesh Nikam <118107698+prath457@users.noreply.github.com>
2026-03-06 17:23:14 +0530

    update base1.java

---
+++ Base1.java
@@ -0,0 +1,30 @@
+package base;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+@SuppressWarnings("unused")
+public class Base1 {
+
+public WebDriver driver;
+
+@BeforeMethod
+public void setup() {
+
+driver = new ChromeDriver();
+
+driver.manage().window().maximize();
+
+driver.get("https://www.saucedemo.com");
+
+}
+
+@AfterMethod
+public void tearDown() {
+
+driver.quit();
+
+}
+
+}
